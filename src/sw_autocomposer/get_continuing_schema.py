0fb1d24a7ea286589369ccbb5fff3b16be34d33d
RJ Salvador <salvador.workshop@gmail.com>
2026-07-28 17:19:50 -0400

    Setting up random drawing functions

---
+++ src/sw_autocomposer/get_continuing_schema.py
@@ -0,0 +1,25 @@
+import argparse
+import os
+
+root_dir = os.path.join(os.path.dirname(__file__), "..", "..")
+output_dir = os.path.join(root_dir, "output")
+
+script_desc = "..."
+
+
+def get_continuin_schema():
+
+    parser = argparse.ArgumentParser(description=script_desc)
+    args = parser.parse_args()
+
+    print("\n----------------------------------------------------------------\n")
+
+    print(f"> Get Continuing Schema")
+    print(f">")
+    print(f"> Test content")
+
+    print("\n----------------------------------------------------------------\n")
+
+
+if __name__ == "__get_continuin_schema__":
+    get_continuin_schema()
